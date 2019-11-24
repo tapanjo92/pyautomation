b02e7624e901c0518bd7854287c64d8ebb9dcdb7
Tapan Joshi <tapan.mjo5892@gmail.com>
2019-11-24 21:52:57 +1100

    boto3 bucket

--- webtron/web/web.py
+++ webtron/web/web.py
@@ -3,5 +3,5 @@ import boto3
 s3 = boto3.resource('s3')
 
 if __name__ == '__main__':
-    for bucket in s3.buckets.all()
-    print (bucket)
+    for bucket in s3.buckets.all():
+        print(bucket)
