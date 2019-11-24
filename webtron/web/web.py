5007fa2f9f3062bff122b8cfee25f77ab8f15108
Tapan Joshi <tapan.mjo5892@gmail.com>
2019-11-24 22:10:38 +1100

    boto3 bucket

--- webtron/web/web.py
+++ webtron/web/web.py
@@ -3,11 +3,16 @@ import click
 
 s3 = boto3.resource('s3')
 
-@click.command('list-buckets')
+@click.group()
+def cli():
+    "Web deploys websites to aws"
+    pass
+
+@cli.command('list-buckets')
 def list_buckets():
     "List all s3 buckets"
     for bucket in s3.buckets.all():
         print(bucket)
 
 if __name__ == '__main__':
-    list_buckets()
+    cli()
